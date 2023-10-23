e01a2e440eb7ed0a54f226a7684a10bf455b9f93
alumno <nagic.galdur@gmail.com>
2023-10-23 09:43:59 +0200

    ejercicios estilos

---
+++ Tema3-Calculadora - copia/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema3_Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, TextChangedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int buttonNumber = int.Parse(clickedButton.Name.Replace("boton", ""));
+            texto.Text = "Texto del botón " + buttonNumber; 
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
